f0d0351d1b29b41bf48f7dd5118b6ff7c728c408
satyaandm <43974317+SatyaanM@users.noreply.github.com>
2020-11-05 18:30:18 -0400

    .

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 import random
-import config
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix='.', intents=intents)
@@ -27,29 +30,29 @@ async def ping(context):
 
 
 # 8ball command
-# @client.command(aliases=['8ball'])
-# async def _8ball(context, *, question):
-#     responses = ['It is certain.',
-#                  'It is decidedly so.',
-#                  'Without a doubt.',
-#                  'Yes - definitely.',
-#                  'You may rely on it.',
-#                  'As I see it, yes.',
-#                  'Most likely.',
-#                  'Outlook good.',
-#                  'Yes.',
-#                  'Signs point to yes.',
-#                  'Reply hazy, try again.',
-#                  'Ask again later.',
-#                  'Better not to tell you now.',
-#                  'Cannot predict now.',
-#                  'Concentrate and ask again.',
-#                  "Don't count on it",
-#                  'My reply is no.',
-#                  'My sources say no.',
-#                  'Outlook not so good.',
-#                  'Very doubtful.']
-#     await context.send(f'Question: {question}\n8 Ball says {random.choice(responses)}')
+@client.command(aliases=['8ball'])
+async def _8ball(context, *, question):
+    responses = ['It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt.',
+                 'Yes - definitely.',
+                 'You may rely on it.',
+                 'As I see it, yes.',
+                 'Most likely.',
+                 'Outlook good.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, try again.',
+                 'Ask again later.',
+                 'Better not to tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 "Don't count on it",
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Very doubtful.']
+    await context.send(f'Question: {question}\n8 Ball says {random.choice(responses)}')
 
 
 @client.command()
@@ -61,11 +64,27 @@ async def clear(context, amount=5):
 @client.command()
 async def kick(context, member: discord.Member, *, reason='None.'):
     await member.kick(reason=reason)
+    await context.send(f'Kicked {member.name}#{member.discriminator}')
 
 
 @client.command()
 async def ban(context, member: discord.Member, *, reason='None.'):
     await member.ban(reason=reason)
+    await context.send(f'Banned {member.name}#{member.discriminator}')
 
 
-client.run(config.token)
+@client.command()
+async def unban(context, *, member):
+    banned_users = await context.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await context.guild.unban(user)
+            await context.send(f'Unbanned {user.name}#{user.discriminator}')
+            return
+
+
+client.run(os.getenv('TOKEN'))
